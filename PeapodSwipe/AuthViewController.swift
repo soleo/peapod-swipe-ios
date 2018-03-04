51312d251ad8dc1e5aec0a3d44033a9cc8fba359
Xinjiang Shao <xinjiang.shao@ahold.com>
2018-03-04 16:35:04 -0600

    Create user account in firebase for storing user info.

--- PeapodSwipe/AuthViewController.swift
+++ PeapodSwipe/AuthViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class AuthViewController: UIViewController {
     
@@ -57,6 +58,7 @@ extension AuthViewController{
             // ...
             // print(user)
            Analytics.logEvent("sign_in", parameters: nil)
+           
            let cardViewController = CardViewController()
            return self.present(cardViewController, animated: true, completion: nil)
         }
@@ -67,15 +69,35 @@ extension AuthViewController{
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         
         alert.addTextField(configurationHandler: { textField in
-            textField.placeholder = "bagel.everything@ahold.com"
+            textField.placeholder = "bagel.is.everything@ahold.com"
         })
         
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
             
-            if let email = alert.textFields?.first?.text {
+            if let email = alert.textFields?.first?.text?.trim() {
                 print("Your email: \(email)")
-                if self.isValidEmail(emalAddress: email.trim()) {
-                    self.SELsignInAnonymously()
+                if self.isValidEmail(emalAddress: email) {
+                    Auth.auth().signIn(withEmail: email , password: "ppod9ppod9", completion: { (user, error) in
+                        if error != nil {
+                            print("Sign In error: \(String(describing: error))")
+                            Auth.auth().createUser(withEmail: email, password: "ppod9ppod9", completion: { (user, error) in
+                                print("Sign In error: \(String(describing: error))")
+                                if error == nil {
+                                    Analytics.logEvent("sign_up", parameters: [ "email": email ])
+                                    let cardViewController = CardViewController()
+                                    return self.present(cardViewController, animated: true, completion: nil)
+                                }
+                            })
+                            
+                        } else {
+                            Analytics.logEvent("sign_in", parameters: [ "email": email ])
+                            let cardViewController = CardViewController()
+                            return self.present(cardViewController, animated: true, completion: nil)
+                        }
+                        
+                        
+                        
+                    })
                 }
             }
         }))
